cfd2a7856279aa583b38487a0221ac597dd87f97
Lane-bk <alottyty@gmail.com>
2026-03-30 20:40:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// печать текста
-const text = "Гарант сделок • Низкая комиссия • Безопасные сделки";
+const text="Гарант сделок • 6% комиссия • Безопасные сделки • 900+ сделок";
 let i=0;
 function type(){
  if(i<text.length){
@@ -10,20 +9,16 @@ function type(){
 }
 type();
 
-// copy TON
 function copyWallet(){
  navigator.clipboard.writeText(
-   document.getElementById("wallet").innerText
+  document.getElementById("wallet").innerText
  );
- alert("Скопировано!");
+ alert("Адрес скопирован!");
 }
 
-// плавное появление карточек
 const observer=new IntersectionObserver(entries=>{
  entries.forEach(entry=>{
-  if(entry.isIntersecting){
-   entry.target.classList.add("show");
-  }
+  if(entry.isIntersecting) entry.target.classList.add("show");
  });
 });
 document.querySelectorAll(".reveal").forEach(el=>observer.observe(el));
